4146a6c1307ea3e1513468dedf58e256e1d8ed49
Akshay Patil <patilakshays1994@gmail.com>
2016-09-19 14:23:04 +0530

    Initial Commit

---
+++ LoginScreen/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LoginScreen
+//
+//  Created by Mac on 19/09/16.
+//  Copyright © 2016 Akshay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LoginScreen/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LoginScreen
+//
+//  Created by Mac on 19/09/16.
+//  Copyright © 2016 Akshay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
